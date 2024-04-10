a14458dfbf1b601abfca3d8e53128801ca0fe332
Ivan Kelembetov <john-1123@narod.ru>
2024-04-10 10:00:19 +0300

    Обновлён шаблон компонента

--- ivan/certificate/templates/main/template.php
+++ ivan/certificate/templates/main/template.php
@@ -3,22 +3,34 @@
 use \Bitrix\Main\Engine\CurrentUser;
 use Bitrix\Main\Localization\Loc;
 
+/** @var array $arParams */
+/** @var array $arResult */
+/** @global CMain $APPLICATION */
+/** @global CUser $USER */
+/** @global CDatabase $DB */
+/** @var CBitrixComponentTemplate $this */
+/** @var string $templateName */
+/** @var string $templateFile */
+/** @var string $templateFolder */
+/** @var string $componentPath */
+/** @var CBitrixComponent $component */
+
 Loc::loadMessages(__FILE__);
 
 ?>
 
 <form method="post">
-    <input type="text" name="certificate" />
+    <input type="text" id="certificate-current" name="certificate" />
     <input type="button" value="Активировать" />
     <input type="hidden" name="user_id" value="<?=CurrentUser::get()->getId() ?>" />
     <?=bitrix_sessid_post()?>
 </form>
 <div><?=Loc::getMessage('IVAN_CERTIFICATES_AVAILABLE') ?></div>
-<ul>
+<ul id="certificates-available">
 <?php
 if ($arResult["AVAILABLE"]->SelectedRowsCount() != 0) {
 while ($element = $arResult["AVAILABLE"]->GetNext()) {
-    echo '    <li><a href="' . $element['DETAIL_PAGE_URL'] . '">' . $element['NAME'] . "</a></li>\n";
+    echo '    <li><a href="javascript:void(0)">' . $element['NAME'] . "</a></li>\n";
 }
 } else {
     echo Loc::getMessage('IVAN_CERTIFICATES_LIST_EMPTY');
